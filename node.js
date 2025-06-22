6fb75e15bfb633e7ca32fe6e50612b23cb83dd5d
Nishanthprabhu1 <nishanthfx4u@gmail.com>
2025-06-22 21:13:47 +0000

    F2

--- node.js
+++ node.js
@@ -1,7 +1,29 @@
-fetch("http://localhost:5678/webhook-test/chatbot1", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ message: "hello" })
-})
-  .then(res => res.json())
-  .then(data => console.log(data));
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Chatbot Test</title>
+</head>
+<body>
+  <h1>Chatbot1 Test</h1>
+  <button onclick="sendMessage()">Send Hello</button>
+
+  <script>
+    function sendMessage() {
+      fetch("http://localhost:5678/webhook-test/chatbot1", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ message: "Hello from browser!" })
+      })
+      .then(res => res.json())
+      .then(data => {
+        alert("Response: " + JSON.stringify(data));
+      })
+      .catch(err => {
+        alert("Error: " + err.message);
+      });
+    }
+  </script>
+</body>
+</html>
